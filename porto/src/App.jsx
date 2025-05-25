43eaab1488c214ee185aa9c756437845b4c1f012
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-05-25 14:11:42 -0700

    mm

--- porto/src/App.jsx
+++ porto/src/App.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import Navbar from './component/Navbar';
-import Header from './component/Header';
-import About from './component/About';
-import Service from './component/Service';
-import Project from './component/Project';
-import Footer from './component/Footer';
+// import React from 'react';
+// import Navbar from './component/Navbar';
+// import Header from './component/Header';
+// import About from './component/About';
+// import Service from './component/Service';
+// import Project from './component/Project';
+// import Footer from './component/Footer';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Header/>
-      <About />
-      <Service />
-      <Project/>
-      <Footer />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar />
+//       <Header/>
+//       <About />
+//       <Service />
+//       <Project/>
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
