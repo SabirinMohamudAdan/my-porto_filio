aad40282da92b743bd1cabafe68f872231d8d7be
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-05-25 14:12:39 -0700

    ...

--- porto/src/App.jsx
+++ porto/src/App.jsx
@@ -22,3 +22,31 @@
 
 // export default App;
 
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import About from './components/About';
+import Service from './components/Service';
+import Project from './components/Project';
+import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App bg-gray-900">
+        <Navbar />
+        <Header />
+        <About />
+        <Service />
+        <Project />
+        <Footer />
+        <Analytics />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
