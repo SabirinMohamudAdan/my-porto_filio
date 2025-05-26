2e69a705edce1029efa68b12564091b33ef1ad61
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-05-25 22:20:42 -0700

    mm

--- porto/src/component/Project.jsx
+++ porto/src/component/Project.jsx
@@ -124,6 +124,7 @@ import { motion } from 'framer-motion';
 import project1 from '../assets/img.jpg';
 import project2 from '../assets/img3.jpg';
 import project3 from '../assets/img6.jpg';
+import project4 from '../assets/rest.jpg';
 
 const projects = [
   {
